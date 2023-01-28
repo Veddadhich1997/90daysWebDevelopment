c9f1dac422688f6433a0cdd01f196fc96601d191
Ved Dadhich <dadhichved1997@gmail.com>
2023-01-28 18:37:31 +0530

    learnt about ternary operator

---
+++ JSTube/ternaryOperator.js
@@ -0,0 +1,9 @@
+var authenticated = false;
+// if(authenticated){
+//     console.log("Show SignOut button");
+// }
+// else{
+//     console.log("Show sign in button");
+// }
+
+console.log(authenticated?"Show SignOut button":"Show sign in button");
