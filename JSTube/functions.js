06ec9cc0af0290b0c26c5c6ca78327d5dbe60031
Ved Dadhich <dadhichved1997@gmail.com>
2023-01-28 19:40:39 +0530

    learnt about functions

---
+++ JSTube/functions.js
@@ -0,0 +1,10 @@
+function ved(name){
+    console.log("Hello How are you "+name);
+}
+ved("Ved");
+
+function rohit(name){
+    return("Hello How are you "+name);
+}
+
+console.log(rohit("Rohit Singh"));
