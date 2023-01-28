6317b27e7a55201151404216d29f31ca53f9389f
Ved Dadhich <dadhichved1997@gmail.com>
2023-01-28 18:44:25 +0530

    learnt about switchCase

---
+++ JSTube/switchCase.js
@@ -0,0 +1,15 @@
+var user = "bat";
+switch(user){
+    case "batsmen":
+        console.log("Hello BatsMan how are you");
+        break;
+    case "bowler":
+        console.log("Hello Bowler how are you");
+        break;
+    case "wicketkeeper":
+        console.log("Hello wicketkeeper how are you");
+        break;
+
+    default:
+        console.log("Hello allrounder how are you");
+}
