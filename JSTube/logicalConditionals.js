a53c564483ff9d7000fa20a62aaf1f79c7bac526
Ved Dadhich <dadhichved1997@gmail.com>
2023-01-28 18:24:57 +0530

    learnt about logical conditionals

---
+++ JSTube/logicalConditionals.js
@@ -0,0 +1,11 @@
+var google = false;
+var facebook = false;
+var email = false;
+
+if(google||facebook||email){
+    console.log("Welcome dear user on your profile");
+}
+
+else{
+    console.log("Dear user please sign up and then come back");
+}
