35079aef919745327f6f5e665217b60926793d63
Ved Dadhich <dadhichved1997@gmail.com>
2023-01-30 11:18:50 +0530

    learnt about context in js

---
+++ JSTube/jsContext.js
@@ -0,0 +1,15 @@
+// getUser();
+
+
+// function getUser(){
+//     console.log("Here is the user");
+// }
+
+// var myName = "Ved";
+// if(myName===myName){
+//     console.log("Hello Ved");
+// }
+
+// if(myName===window.myName){
+//     console.log("Hello Ved");
+// }
