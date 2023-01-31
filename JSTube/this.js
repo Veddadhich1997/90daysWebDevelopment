2643b3fa865c157e13288a9d356cac2425f7ee37
Ved Dadhich <dadhichved1997@gmail.com>
2023-01-31 17:37:17 +0530

    learnt about this keyword

---
+++ JSTube/this.js
@@ -0,0 +1,3 @@
+// console.log(this);
+
+// this keyword provides the access of global execution.in the case of node it will provide empty scope for ex.{} and in the case of browser it will provide the access of window as global execution. 
