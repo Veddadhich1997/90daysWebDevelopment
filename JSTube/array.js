2c6f348f3e30246c5df2e6fcd2721db04e76de5d
Ved Dadhich <dadhichved1997@gmail.com>
2023-01-31 19:34:25 +0530

    learnt about array and its methods

---
+++ JSTube/array.js
@@ -0,0 +1,29 @@
+var country = ["India","Japan","England","America"];
+var states = new Array("Rajasthan","Delhi","UP","Haryana");
+
+// console.log(states);
+// console.log(states[0]);
+
+// console.log(states.length);
+
+// console.log(states.indexOf("Rajasthan"));
+// console.log(states.indexOf("Delhi"));
+
+// states[0]="Punjab";
+// console.log(states);
+
+// console.log(states.pop());
+// console.log(states);
+
+// states.push("ved");
+// console.log(states);
+
+// states.unshift("Ved Dadhich");
+// console.log(states);
+
+// states.shift();
+// console.log(states);
+
+// console.log(states.indexOf("vpo"));
+
+// console.log(Array.from("Ved Dadhich"));
