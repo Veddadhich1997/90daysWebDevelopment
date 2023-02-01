330ea6cec966e44516f1489f764989c09ca59fc4
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-01 21:05:37 +0530

    learnt about while loop

---
+++ JSTube/while.js
@@ -0,0 +1,24 @@
+var states = new Array(
+    "Rajasthan",
+    "Haryana",
+    "Punjab",
+    1997,
+    "UP"
+    );
+
+
+    // let i = 0;
+
+    // while(i<states.length){
+    //     if(typeof states[i]!="string") break;
+    //     console.log(states[i]);
+    //     i++;
+    // }
+
+
+    let i = 0;
+    do{
+        console.log(i);
+        i++;
+    }
+    while(i<=10);
