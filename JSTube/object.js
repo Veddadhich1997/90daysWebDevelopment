02abb3e50fb4007728d86a30c94cb30df7c808a8
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-01 12:09:18 +0530

    learnt about objects in js

---
+++ JSTube/object.js
@@ -0,0 +1,11 @@
+var user = {
+    firstName : "Ved",
+    lastName : "Dadhich",
+    phoneNumber : 789456642,
+    fb : true
+}
+
+// console.log(user);
+// console.table(user);
+console.log(user.firstName);
+console.log(user["lastName"]);
