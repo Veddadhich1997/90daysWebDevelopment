6f947aa8a233f962a901f394dfed87a102a9f2fb
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-01 10:55:20 +0530

    learnt about arrow function and callbacks

---
+++ JSTube/arraypt2.js
@@ -0,0 +1,38 @@
+// function isEven(element){
+//     if(element%2===0){
+//         return true;
+//     }
+//     return false;
+// }
+
+// console.log(isEven(3));
+
+// function isEven(element){
+//     if(element%2===0){
+//         return true;
+//     }
+//     return false;
+//     return element%2===0;
+// }
+
+// console.log(isEven(2));
+
+
+// var isEven = (element)=>{
+//    return element%2===0;
+// }
+
+// console.log(isEven(2));
+
+// var isEven = (element)=>(
+//     element%2===0
+// )
+
+// console.log(isEven(2));
+
+// var x = [2,5,6,8].every(isEven);
+// console.log(x);
+
+
+// var x = [2,4,6,8].every((e)=>(e%2===0));
+// console.log(x);
