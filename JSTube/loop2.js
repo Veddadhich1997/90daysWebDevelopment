96aadc0521b7412909b78f0ea89016ef84f28a4e
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-02 00:58:20 +0530

    learnt about for of and for in loop

---
+++ JSTube/loop2.js
@@ -0,0 +1,15 @@
+// var names = new Array("Youtube","facebook","twitter","gmail");
+// for(const n of names){
+//     console.log(n);
+// }
+
+var symbols = {
+    yt : "youtube",
+    fb : "facebook",
+    ig : "instagram",
+    tw : "twitter"
+}
+
+for(const i in symbols){
+    console.log(i);
+}
