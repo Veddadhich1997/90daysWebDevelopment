f3e37e1bda7c3c1e02e6cdacb0047d3e24496995
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-01 11:56:16 +0530

    learnt about slice and splice method in array

--- JSTube/fillFilter.js
+++ JSTube/fillFilter.js
@@ -1,6 +1,36 @@
-var arra = [10,11,12,13];
-var arr = new Array(10,11,12,13);
-console.log(arr.fill("ved",1,3));
+// var arra = [10,11,12,13];
+// var arr = new Array(10,11,12,13);
+// console.log(arr.fill("ved",1,3));
 
-var arr = new Array(11,12,13,14);
-console.log(arr.filter((e)=>e>12));
+// var arr = new Array(11,12,13,14);
+// console.log(arr.filter((e)=>e>12));
+
+
+var city = new Array("Jhunjhunu","Jaipur","Sikar","Churu");
+// console.log(city.length);
+// console.log(city[0]);
+// city[2] = "Noida";
+// console.log(city);
+// console.log(city.indexOf("Noida"));
+// city.pop();
+// console.log(city);
+// city.push("Noida");
+// console.log(city);
+// city.shift();
+// console.log(city);
+// city.unshift("Noida");
+// console.log(city);
+// console.log(Array.from("VedDadhich"));
+// var arr = [10,11,12,13,14];
+// var x =arr.every((e)=>e<13);
+// console.log(x);
+
+// city.fill("Noida",1,3);
+// console.log(city);
+
+// city.filter((e)=>e!=Jaipur);
+// console.log(city);
+
+// console.log(city.slice(1,3));
+// city.splice(1,2,"Ved");
+// console.log(city);
---
+++ JSTube/sliceSplice.js
@@ -0,0 +1,7 @@
+// var arr = [10,12,14,16,18];
+// var ar = arr.slice(1,3);
+// console.log(ar);
+
+var arr = [10,12,14,16,18];
+arr.splice(1,3,"Ved");
+console.log(arr);
