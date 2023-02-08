403dd62dd1fe5b5e3627f0537356a14fdca94f7e
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-08 11:25:12 +0530

    learnt about string literals

---
+++ JSTube/stringLiterals.js
@@ -0,0 +1,6 @@
+var name = `Ved`;
+var fullName = `${name} Dadhich`;
+console.log(fullName);
+console.log(fullName.charAt(3));
+console.log(fullName.toUpperCase());
+console.log(fullName.includes("Dadh"));
