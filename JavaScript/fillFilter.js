9847292de948a29355aac65fe8cc2a6f6c2920b3
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-09 19:38:58 +0530

    learnt about fill filter and slice splice methods

---
+++ JavaScript/fillFilter.js
@@ -0,0 +1,18 @@
+var array = [2,4,6,8,10];
+// var x=array.every((e)=>e%2==0);
+// console.log(x);
+
+// console.log(array.filter((e)=>e<8));
+
+array.fill("ved",1,3);
+console.log(array);
+
+
+// Slice Splice
+
+// console.log(array.slice(1,4));
+
+var arr = [10,12,14,16,18];
+arr.splice(1,3,"Ved");
+console.log(arr);
+
