38630b1f270d1c802a267d50afe09f700a713bc4
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-09 11:05:19 +0530

    learnt about switch case

---
+++ JavaScript/switchCase.js
@@ -0,0 +1,14 @@
+var user ;
+switch(user){
+    case "admin":
+        console.log("Hello Admin");
+        break;
+    
+    case "subadmin":
+        console.log("Hello SubAdmin");
+        break;
+
+    default:
+        console.log("Hello user");
+        break;
+}
