a821af3d6ac2efe929c373ab26eaed499713bd7a
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-09 11:32:33 +0530

    learnt about basic function

---
+++ JavaScript/function.js
@@ -0,0 +1,20 @@
+// function sum(){
+//     console.log("Hello Ved Dadhich");
+// }
+// sum();
+
+// function printName(name){
+//     console.log(`Hello Your Name is:${name}`);
+// }
+
+// printName("Ved Dadhich");
+
+function printName(name){
+    return `Hello Your Name is:${name}`;
+}
+
+var vpd = printName("Ved Dadhich");
+console.log(vpd);
+
+
+
