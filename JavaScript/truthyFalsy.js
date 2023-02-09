056cfcbad88da46f5919b5575d128799b28fab98
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-09 11:25:10 +0530

    learnt about truthy and falsy values

---
+++ JavaScript/truthyFalsy.js
@@ -0,0 +1,21 @@
+// null
+// undefined
+// NaN
+// ""
+// 0
+
+var value = null;
+
+if(value){
+    console.log("this is true");
+}
+
+var value = "20";
+
+if(value==20){
+    console.log("this is true");
+}
+
+if(value===20){
+    console.log("this is true");
+}
