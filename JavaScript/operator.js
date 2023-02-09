9320264f12660091720243d4f2d24919ee27f68d
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-09 10:40:59 +0530

    revising the topics

---
+++ JavaScript/operator.js
@@ -0,0 +1,4 @@
+var sellingPrice = 199;
+var lisitingPrice = 799;
+var discount = ((lisitingPrice-sellingPrice)/lisitingPrice)*100;
+console.log(Math.round(discount));
---
+++ JavaScript/variable.js
@@ -0,0 +1,18 @@
+const uid = "abc12";
+var firstName = "Ved";
+var lastName = "Dadhich";
+var email = "ved@gmail.com";
+var courseCount = 4;
+var isLoggedFromFB = true;
+
+// console.log("Your firstName is: " + firstName);
+// console.log("Your firstName is: " , firstName);
+
+console.log(`
+    Your UID is: ${uid}
+    Your FirstName is: ${firstName}
+    Your LastName is: ${lastName}
+    Your Email is: ${email}
+    Your CourseCount is: ${courseCount}
+    You are logged from fb: ${isLoggedFromFB}
+`);
