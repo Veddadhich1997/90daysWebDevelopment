c12b55e484402531d3e6169fe71b0356a1b78618
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-09 17:11:21 +0530

    learnt about array and its method and jscontext

---
+++ JavaScript/array.js
@@ -0,0 +1,19 @@
+// const states = ["Rajasthan","Haryana","Delhi","UP"];
+// console.log(states);
+
+const statee = new Array("Rajasthan","Haryana","Delhi","UP");
+// console.log(statee);
+// console.log(statee[1]);
+// console.log(statee.length);
+
+// console.log(statee.indexOf("UP"));
+// console.log(statee.pop());
+// console.log(statee);
+// statee.push("Punjab");
+// console.log(statee);
+// statee.shift();
+// console.log(statee);
+// statee.unshift("Punjab");
+// console.log(statee);
+
+console.log(Array.from("VedDadhich"));
