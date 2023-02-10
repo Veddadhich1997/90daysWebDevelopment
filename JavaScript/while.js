dab176b0bdb1eca4ca5ed2c0da602d250fda651f
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-10 17:02:39 +0530

    learnt about while loop

---
+++ JavaScript/while.js
@@ -0,0 +1,45 @@
+// var x = 0;
+// while(x<10){
+//     console.log(x);
+//     x++;
+// }
+
+// var states = new Array(
+//     "Rajasthan",
+//     "Haryana",
+//     "Punjab",
+//     1997,
+//     "UP"
+//     );
+
+
+//     let i =0;
+//     while(i<=states.length){
+//         if(typeof states[i]!="string") break;
+//         console.log(states[i]);
+//         i++;
+//     }
+
+// let i =0;
+// while(i<states.length){
+//     console.log(states[i]);
+//     i++;
+// }
+
+// do{
+//     console.log(states[i]);
+//     i++;
+// }
+// while(i<states.length);
+
+
+var states = new Array(
+    "Rajasthan",
+    "Haryana",
+    "Punjab",
+    1997,
+    "UP"
+    );
+
+
+    // states.forEach((e)=>console.log(e));
